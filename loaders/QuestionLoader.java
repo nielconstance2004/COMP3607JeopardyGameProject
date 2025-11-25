3b6c71a9c8055d61c74ac1530547c5222b74ee6f
nielconstance2004 <nielconstancework@gmail.com>
2025-11-24 21:45:04 -0400

    Deleted Files and Folders, because I put them in the src folder

--- loaders/QuestionLoader.java
+++
@@ -1,9 +0,0 @@
-package com.jeopardy.loader;
-import com.jeopardy.model.Question;
-import java.io.File;
-import java.util.List;
-
-public interface QuestionLoader {
-    List<Question> load(File file) throws Exception;
-}
-
--- scoring/ScoringStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.jeopardy.scoring;
-
-import com.jeopardy.model.Player;
-import com.jeopardy.model.Question;
-
-public interface ScoringStrategy {
-
-    int scoreForAnswer(Player player, Question question, String givenAnswer);
-}
